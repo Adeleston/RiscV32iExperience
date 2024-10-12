e3a81c8a91cec2bc11b09c79979765f6050d118e
Emine TENGİR <157812274+Tengirella@users.noreply.github.com>
2024-10-12 04:34:26 +0300

    Add files via upload

---
+++ rtl/reg_file.v
@@ -0,0 +1,29 @@
+module reg_file(
+    input [31:0] inst,
+    input [31:0] busW,
+    input RegWr,
+    input Wr_Clk,
+    output [31:0] rs1,
+    output [31:0] rs2
+    );
+
+    reg [4:0] Ra;
+    reg [4:0] Rb;
+    reg [4:0] Rw;
+    reg [31:0] register [31:0];
+    
+    always@(*) begin
+        Ra = inst[19:15]; 
+        Rb = inst[24:20]; 
+        Rw = inst[11:7];  
+    end
+    
+    assign rs1 = register[Ra];
+    assign rs2 = register[Rb];
+    
+    always @(posedge Wr_Clk) begin
+        if (RegWr && (Rw != 0))
+            register[Rw] <= busW;
+        end
+    
+endmodule
