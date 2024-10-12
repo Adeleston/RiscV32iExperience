42894ab27484e84f5aaa18241c155abb1a18ea03
Emine TENGİR <157812274+Tengirella@users.noreply.github.com>
2024-10-12 04:53:50 +0300

    Update adder.v

--- rtl/adder.v
+++ rtl/adder.v
@@ -1,7 +1,7 @@
 module adder(
     input [31:0] mux_1out,
     input [31:0] mux_2out,
-    output reg [31:0] NextPc
+    output reg [31:0] NextPc        //taşma durumu oalbilir mi bakalım
     );
     
     always@(*)begin
