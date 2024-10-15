b9732c18c23c2341cd2a6c308ccf7523074b25d9
Emine TENGİR <157812274+Tengirella@users.noreply.github.com>
2024-10-15 20:15:14 +0300

    Update imm_gen.v

--- rtl/imm_gen.v
+++ rtl/imm_gen.v
@@ -1,7 +1,8 @@
 module imm_gen (
     input [31:0] inst,  
     input [2:0] ExtOp,        
-    output reg [31:0] i_imm,    //addi,slti,sltu,xori,ori,andi,lb,lh,lw,lbu,lhu,jalr
+    output reg [31:0] i_imm,    //addi,slti,sltu,xori,ori,andi,
+    output reg [31:0] l_imm,   //lb,lh,lw,lbu,lhu,jalr
     output reg [31:0] b_imm,    //beq,bne,blt,bge,bltu,bgeu
     output reg [31:0] j_imm,    //jal
     output reg [31:0] s_imm,    //sb,sh,sw
@@ -15,7 +16,8 @@ module imm_gen (
                 3'b010: j_imm = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0};      
                 3'b011: s_imm = {{21{inst[31]}}, inst[30:25], inst[11:7]};                    
                 3'b100: ii_imm = {inst[31:12], 12'b0};
-                3'b101: shamt = inst[24:20];
+                3'b101: l_imm = {{21{inst[31]}}, inst[30:20]}; 
+                3'b110: shamt = inst[24:20];
                 default: begin
                     i_imm = 32'b0;
                     b_imm = 32'b0;
