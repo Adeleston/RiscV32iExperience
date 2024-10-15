fee46f18e2bd8d666a118a465dd66d674bf632ee
Emine TENGİR <157812274+Tengirella@users.noreply.github.com>
2024-10-15 21:25:01 +0300

    Update imm_gen.v

--- rtl/imm_gen.v
+++ rtl/imm_gen.v
@@ -1,29 +1,16 @@
 module imm_gen (
     input [31:0] inst,  
     input [2:0] ExtOp,        
-    output reg [31:0] i_imm,    //addi,slti,sltu,xori,ori,andi,lb,lh,lw,lbu,lhu,jalr
-    output reg [31:0] b_imm,    //beq,bne,blt,bge,bltu,bgeu
-    output reg [31:0] j_imm,    //jal
-    output reg [31:0] s_imm,    //sb,sh,sw
-    output reg [31:0] ii_imm,    //lui,aıpic
-    output reg [4:0] shamt
+    output reg [31:0] imm_out,  
     );
         always@(*) begin
             case (ExtOp)
-                3'b000: i_imm = {{21{inst[31]}}, inst[30:20]};                                             
-                3'b001: b_imm = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0};            //1'b0 hizalama biti           
-                3'b010: j_imm = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0};      
-                3'b011: s_imm = {{21{inst[31]}}, inst[30:25], inst[11:7]};                    
-                3'b100: ii_imm = {inst[31:12], 12'b0};
-                3'b101: shamt = inst[24:20];
-                default: begin
-                    i_imm = 32'b0;
-                    b_imm = 32'b0;
-                    j_imm = 32'b0;
-                    s_imm = 32'b0;
-                    ii_imm = 32'b0;
-                    shamt = 32'b0;
-                end
+                3'b000: imm_out = {{21{inst[31]}}, inst[30:20]};  //addi,slti,sltu,xori,ori,andi,lb,lh,lw,lbu,lhu,jalr                                           
+                3'b001: imm_out = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0}; //beq,bne,blt,bge,bltu,bgeu          
+                3'b010: imm_out = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0}; //jal     
+                3'b011: imm_out = {{21{inst[31]}}, inst[30:25], inst[11:7]};  //sb,sh,sw                  
+                3'b100: imm_out = {inst[31:12], 12'b0}; //lui,aıpic
+                default: imm_out = 32b'0;
             endcase
         end
 endmodule
