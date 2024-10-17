7596f648990497aaea478da4a22f6495f9d3cd12
abdumatin-asoev <115032620+abdumatin-asoev@users.noreply.github.com>
2024-10-17 21:49:59 +0300

    Update muxes.v

--- rtl/muxes.v
+++ rtl/muxes.v
@@ -3,7 +3,7 @@ input [31:0] imm,
 input PCASRC,
 output [31:0] mux_1out
 );
-assign mux_1out = PCASRC?imm:32'h00000004;
+assign mux_1out = PCASRC?32'h00000004:imm;
 endmodule
 module mux2x1_2(
 input [31:0] rs1,
@@ -11,7 +11,7 @@ input [31:0] pcountervalue,
 input PCBSRC,
 output [31:0] mux_2out
 );
-assign mux_2out = PCBSRC?pcountervalue:rs1;
+assign mux_2out = PCBSRC?rs1:pcountervalue;
 endmodule
 module mux2x1_3(
 input [31:0] rs1,
@@ -19,18 +19,18 @@ input [31:0] pcountervalue,
 input ALUAsrc,
 output [31:0] mux_3out
 );
-assign mux_3out = ALUAsrc?rs1:pcountervalue;
+assign mux_3out = ALUAsrc?pcountervalue:rs1;
 endmodule
 module mux3x1_4(
 input [31:0] rs2,
-input [31:0] pcountervalue,
+input [31:0] imm_v,
 input [1:0] ALUBsrc,
 output reg [31:0] mux_4out
 );
 always @(*) begin
     case(ALUBsrc)
         2'b00: mux_4out = 32'h00000004;     // Default increment
-        2'b01: mux_4out = pcountervalue;              // Immediate
+        2'b01: mux_4out = imm_v;              // Immediate
         2'b10: mux_4out = rs2;              // Register value (rs2)
         default: mux_4out = 32'h00000000;   // Safe default case
     endcase
