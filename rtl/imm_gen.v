a5d1f56371d0725315f685f04f6354b65d173f1d
abdumatin-asoev <115032620+abdumatin-asoev@users.noreply.github.com>
2024-10-17 21:45:47 +0300

    Update imm_gen.v

--- rtl/imm_gen.v
+++ rtl/imm_gen.v
@@ -1,16 +1,16 @@
 module imm_gen (
-    input [31:0] inst,  
+    input [31:7] inst,  
     input [2:0] ExtOp,        
-    output reg [31:0] imm
+    output reg [31:0] imm_out    // ??k?? olarak tek bir immediate de?eri kullan
     );
-        always@(*) begin
-            case (ExtOp)
-                3'b000: imm = {{21{inst[31]}}, inst[30:20]};  //addi,slti,sltu,xori,ori,andi,lb,lh,lw,lbu,lhu,jalr                                           
-                3'b001: imm = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0}; //beq,bne,blt,bge,bltu,bgeu          
-                3'b010: imm = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0}; //jal     
-                3'b011: imm = {{21{inst[31]}}, inst[30:25], inst[11:7]};  //sb,sh,sw                  
-                3'b100: imm = {inst[31:12], 12'b0}; //lui,aıpic
-                default: imm = 32'b0;
-            endcase
-        end
+
+    always @(*) begin
+        case (ExtOp)
+            3'b000: imm_out = {{21{inst[31]}}, inst[30:20]}; //I-type
+            3'b010: imm_out = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0}; //b-type
+            3'b111: imm_out = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0};//j-type
+            3'b001: imm_out = {{21{inst[31]}}, inst[30:25], inst[11:7]}; //s-type
+            3'b100: imm_out = {inst[31:12], 12'b0}; //lui aiupc
+        endcase
+    end
 endmodule
