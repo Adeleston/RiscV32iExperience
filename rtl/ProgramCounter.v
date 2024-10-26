49fa9ebf91d80ade727791d37a9b8ad69c42bde6
Yiğit Keleş <115139547+Adeleston@users.noreply.github.com>
2024-10-26 18:57:16 +0300

    Update ProgramCounter.v

--- rtl/ProgramCounter.v
+++ rtl/ProgramCounter.v
@@ -1,14 +1,15 @@
 `timescale 1ns / 1ps
 module programcounter(
     input [31:0] NextPc,
-    input clk,          
+    input clk,
+    input resetn,          
     output reg [31:0] Pc  
 );
-initial begin
-    Pc = 32'h00000000;
-end
 always @(negedge clk) begin
-    if(NextPc)begin
+    if(!resetn)begin
+        Pc = 32'h00000000;
+    end
+    else begin
     Pc <= NextPc;
     end
 end
